09692169dfe4d8403cb7497ea5157000853830c9
RishabhVerma098 <vermarishabh0987@gmail.com>
2023-02-13 23:22:26 +0530

    ci: fix config file for vite for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  base: '/super-wordle/',
   test: {
     globals: true,
     environment: 'jsdom',
